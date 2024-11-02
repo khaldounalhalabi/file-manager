aea4ab2ad42e37ee244531393462d02e61fa1414
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-11-02 21:13:31 +0300

    fixes

--- routes/v1/web/customer.php
+++ routes/v1/web/customer.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\WEB\v1;
 use Illuminate\Support\Facades\Route;
 
+Route::inertia('/v1/customer/', 'dashboard/Index')->name('v1.web.customer.index');
 
 Route::put('/v1/customer/update-user-data', [v1\CustomerAuthController::class, 'updateUserData'])->name('v1.web.customer.update.user.data');
 Route::get('/v1/customer/user-details', [v1\CustomerAuthController::class, 'userDetails'])->name('v1.web.customer.user.details');
